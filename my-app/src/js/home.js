16d2ff8ae4094956adfa98a22fad1f5fd9ee5048
AaronBolarinho <aaron.bolarinho@gmail.com>
2019-07-26 15:10:43 -0400

    added fonts and animations

--- my-app/src/js/home.js
+++ my-app/src/js/home.js
@@ -11,17 +11,42 @@ class Home extends Component {
     }
   }
 
+  renderTitle = () => {
+  	let title = 
+  	<div className='d-flex container'>
+  		<div>
+	    	<video id='background-video' loop autoPlay muted className='video'>
+	          <source src={Video} type='video/mp4' />
+	        </video>
+        </div>
+	  	<div className="centerText title textWhite">
+	  		<div className="animated infinite wobble delayAnimation displayInline">
+	  			<span>&#93;</span>
+	  		</div>
+	  		<div className="displayInline">
+	  			<span> Summer Recess</span>
+	  		</div>
+	  		<div className="animated infinite heartBeat delayAnimation displayInline">
+	  			<span>&#125;</span>
+	  		</div>
+	  		<div>
+	  			<p> for Ontario's Parliment</p>
+	  		</div>
+	  	</div>
+  	</div>
+
+  	return title
+  }
+
   componentDidMount() {
   }
 
   render() {
     console.log('This is the state', this.state)
       return (
-        <div>
-        	<video id='background-video' loop autoPlay muted className='video'>
-	          <source src={Video} type='video/mp4' />
-	        </video>
-        </div>
+      	<div>
+	      {this.renderTitle()}
+	    </div>
       )
   }
 }
