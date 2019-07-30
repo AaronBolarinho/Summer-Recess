cca6e8d97b27e2165aae64085baac5935a7d6970
AaronBolarinho <aaron.bolarinho@gmail.com>
2019-07-30 15:21:30 -0400

    added second and third strip - about to add haiku form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/App.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
